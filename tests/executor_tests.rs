ad70e334a4ce499f7783facd9c489c3082c4298e
TAYTS <taytzushieh@gmail.com>
2026-09-26 06:22:04 +0800

    chore: move ping pong spawn example to executor test

--- tests/executor_tests.rs
+++ tests/executor_tests.rs
@@ -1,5 +1,6 @@
 use mini_rt::{block_on, spawn};
 use ntest::timeout;
+use std::future::poll_fn;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
@@ -104,4 +105,84 @@ fn test_wake_from_other_thread() {
 
     // Ensure the spawned future has driven to completion
     assert!(block_on(async { join_handle.await }));
+}
+
+#[test]
+#[timeout(1000)]
+fn test_ping_pong() {
+    let _ = block_on(async {
+        let shared_data: Arc<Mutex<ShareData>> = Arc::new(Mutex::new(ShareData {
+            data: false,
+            waker: None,
+        }));
+        let clone1 = shared_data.clone();
+        let clone2 = shared_data.clone();
+
+        let output: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::<String>::new()));
+        let out_clone1 = output.clone();
+        let out_clone2 = output.clone();
+
+        let fut1 = spawn(async move {
+            let mut count = 0;
+            while count < 2 {
+                poll_fn(|cx| {
+                    let mut guard = clone1.lock().unwrap();
+
+                    if !guard.data {
+                        guard.data = true;
+                        let waker = guard.waker.take();
+
+                        drop(guard);
+
+                        if let Some(waker) = waker {
+                            waker.wake();
+                        }
+                        return Poll::Ready(());
+                    } else {
+                        guard.waker.replace(cx.waker().clone());
+                    }
+
+                    Poll::Pending
+                })
+                .await;
+                count += 1;
+                let mut guard = out_clone1.lock().unwrap();
+                guard.push("ping".to_string());
+            }
+        });
+        let fut2 = spawn(async move {
+            let mut count = 0;
+            while count < 2 {
+                poll_fn(|cx| {
+                    let mut guard = clone2.lock().unwrap();
+
+                    if guard.data {
+                        guard.data = false;
+                        let waker = guard.waker.take();
+
+                        drop(guard);
+
+                        if let Some(waker) = waker {
+                            waker.wake();
+                        }
+                        return Poll::Ready(());
+                    } else {
+                        guard.waker.replace(cx.waker().clone());
+                    }
+                    Poll::Pending
+                })
+                .await;
+                count += 1;
+                let mut guard = out_clone2.lock().unwrap();
+                guard.push("pong".to_string());
+            }
+        });
+
+        let _ = fut1.await;
+        let _ = fut2.await;
+
+        let output_guard = output.lock().unwrap();
+        println!("{:?}", output_guard);
+        assert_eq!(*output_guard, ["ping", "pong", "ping", "pong"]);
+    });
 }
